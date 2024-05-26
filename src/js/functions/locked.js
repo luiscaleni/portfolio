6bdba03c052d1d0985879cd255d74c50d0e11d45
Luis Caleni <luis.caleni@gmail.com>
2024-05-26 02:28:58 -0300

    adpatacion para celulares-2

--- src/js/functions/locked.js
+++ src/js/functions/locked.js
@@ -4,44 +4,32 @@ window.addEventListener("load", ()=>{
         lockScreen.classList.add("lockScreen")
         document.getElementById("hour").textContent=""
         document.getElementById("barBottomInfo").style.backgroundColor="white"
-    /*
-    document.getElementById("button_lock").addEventListener("click", ()=> { 
-        if(lockScreen.getAttribute("class") === "lockScreen"){
+        
+        function fullScreen(element) {
+            if (element.requestFullScreen) {
+                element.requestFullScreen();
+                localVideo.volume = 0.8;
+                localVideo.play();
+            } else if (element.mozRequestFullScreen) {
+                element.mozRequestFullScreen();
+                localVideo.volume = 0.8;
+                localVideo.play();
+            } else if (element.webkitRequestFullScreen) {
+                element.webkitRequestFullScreen();
+                localVideo.volume = 0.8;
+                localVideo.play();
+            }
+        }
+
+        document.getElementById("unlockOption").addEventListener("click", ()=> {
             lockScreen.classList.add("unlockScreen")
             lockScreen.classList.remove("lockScreen")
-            if(document.getElementById("nav").getAttribute("class") === "" && document.getElementById("contFSReplace") !== null){
-                setTimeout(() => {
-                    document.getElementById("nav").classList.add("black_blur")
-                }, 1000);
-            }
-            
+            navigator.wakeLock.request('screen')
+            fullScreen(document.documentElement)
             setTimeout(() => {
                 lockScreen.style.display="none"
                 document.getElementById("barBottomInfo").style.backgroundColor="transparent"
-            }, 800)
-        }else if (lockScreen.getAttribute("class") === "unlockScreen") {
-            lockScreen.style.display="flex"
-            lockScreen.classList.add("lockScreen")
-            lockScreen.classList.remove("unlockScreen")
-            if(document.getElementById("nav").getAttribute("class") === "black_blur"){
-                document.getElementById("nav").classList.remove("black_blur")
-            }
-            setTimeout(() => {
-                document.getElementById("hour").textContent=""
-                document.getElementById("barBottomInfo").style.backgroundColor="white"
-            }, 100)
-        }else{
-            console.log("error lockScreen")
-        }
-    })*/
-
-    document.getElementById("unlockOption").addEventListener("click", ()=> {
-        lockScreen.classList.add("unlockScreen")
-        lockScreen.classList.remove("lockScreen")
-        setTimeout(() => {
-            lockScreen.style.display="none"
-            document.getElementById("barBottomInfo").style.backgroundColor="transparent"
-        }, 1000);
-    })
-}, 1000)
+            }, 1000);
+        })
+    }, 1000)
 })
