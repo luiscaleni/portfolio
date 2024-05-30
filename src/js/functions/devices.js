98590ac832597fdc83f1ccaac2c33df0c7b7ebc0
Luis Caleni <luis.caleni@gmail.com>
2024-05-30 18:47:46 -0300

    correccion de errores-2

--- src/js/functions/devices.js
+++ src/js/functions/devices.js
@@ -1,6 +1,10 @@
-let root = document.getElementById("root")
 let loaderPage = document.getElementById("loaderPage")
-let nomobile = '<div id="content-general"><div id="content"><p><span id="metadatos">#metadatos</span> es un proyecto que está diseñado para ejecutarse en dispositivos móbiles. ¿Estás en un dispositivo mobil?. <a id="reload" href="index.html" title="Recargar la página">Recarga la página.</a></p></div></div>'
+let root = document.getElementById("root")
+let main = document.getElementById("main")
+let divContent = document.createElement("div")
+let nomobile = '<p id="nomobile-text"><span id="metadatos">#metadatos</span> es un proyecto que está diseñado para ejecutarse en dispositivos móbiles. ¿Estás en un dispositivo mobil?. <a id="reload" href="index.html" title="Recargar la página">Recarga la página.</a></p>'
+divContent.id="content"
+divContent.innerHTML=nomobile
 
 if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
     console.log("Estás usando un dispositivo móvil!!")
@@ -16,6 +20,6 @@ if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i)
     }
 } else {
     console.log("No estás usando un móvil")
-    root.remove()
-    root.appendChild(nomobile)
+    root.appendChild(divContent)
+    root.removeChild(main)
 }
